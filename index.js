e39f001bd6a078434fa1d5f53b4919ac9d6af3f2
Derek S <dschwan21@me.com>
2019-07-08 12:03:30 -0700

    first commit, GET users done

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // implement your API here
+const express = require("express");
+const Db = require("./data/db");
+
+const server = express();
+
+server.get("/", (req, res) => {
+  res.send("Monday July 8 WebAPI Challenge");
+});
+
+server.get("/api/users", function(req, res) {
+  Db.find()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(error => {
+      res
+        .status(500)
+        .json({ error: "The users information could not be retrieved." });
+    });
+});
+
+server.get("/api/users/:id", function(req, res) {
+  Db.find()
+    .then(user => {
+      res.status(200).json(user);
+    })
+    .catch(error => {
+      res
+        .status(404)
+        .json({ message: "The user with the specified ID does not exist." });
+    });
+});
+
+const port = 8000;
+server.listen(port, () => console.log(`\n*** running on port ${port} ***\n`));
