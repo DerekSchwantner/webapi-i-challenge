de6ba5be5c8153056f60d6b71e178384d3c8704d
Derek S <dschwan21@me.com>
2019-07-08 13:11:44 -0700

    CRD operations are working

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const Db = require("./data/db");
 
 const server = express();
 
+server.use(express.json());
+
 server.get("/", (req, res) => {
   res.send("Monday July 8 WebAPI Challenge");
 });
@@ -21,7 +23,9 @@ server.get("/api/users", function(req, res) {
 });
 
 server.get("/api/users/:id", function(req, res) {
-  Db.find()
+  const { id } = req.params;
+  console.log(id);
+  Db.findById(id)
     .then(user => {
       res.status(200).json(user);
     })
@@ -29,6 +33,45 @@ server.get("/api/users/:id", function(req, res) {
       res
         .status(404)
         .json({ message: "The user with the specified ID does not exist." });
+    });
+});
+
+server.post("/api/users", (req, res) => {
+  const userInfo = req.body;
+  console.log(userInfo);
+
+  Db.insert(userInfo)
+    .then(user => {
+      if (user) {
+        res.status(204).send(user);
+      } else {
+        res
+          .status(400)
+          .json({ errorMessage: "Please provide name and bio for the user." });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({
+        error: "There was an error while saving the user to the database"
+      });
+    });
+});
+
+server.delete("/api/users/:id", (req, res) => {
+  const { id } = req.params;
+
+  Db.remove(id)
+    .then(deleted => {
+      if (deleted) {
+        res.status(204).end();
+      } else if (!id) {
+        res
+          .status(404)
+          .json({ message: "The user with the specified ID does not exist." });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ error: "The user could not be removed" });
     });
 });
 
