5ec31a2b49a47e80f4d83a3f71db0dae98689f52
Derek S <dschwan21@me.com>
2019-07-09 13:00:18 -0700

    fixed suggested errors from 1:1

--- index.js
+++ index.js
@@ -39,22 +39,29 @@ server.get("/api/users/:id", function(req, res) {
 server.post("/api/users", (req, res) => {
   const userInfo = req.body;
   console.log(userInfo);
-
-  Db.insert(userInfo)
-    .then(user => {
-      if (user) {
-        res.status(204).send(user);
-      } else {
-        res
-          .status(400)
-          .json({ errorMessage: "Please provide name and bio for the user." });
-      }
-    })
-    .catch(error => {
-      res.status(500).json({
-        error: "There was an error while saving the user to the database"
+  if (!userInfo.name || !userInfo.bio) {
+    res
+      .status(400)
+      .json({ errorMessage: "Please provide name and bio for the user." });
+  } else {
+    Db.insert(userInfo)
+      .then(user => {
+        Db.findById(user.id)
+          .then(data => {
+            res.status(201).json(data);
+          })
+          .catch(error => {
+            res.status(500).json({
+              error: "There was an error while saving the user to the database"
+            });
+          });
+      })
+      .catch(error => {
+        res.status(500).json({
+          error: "There was an error while saving the user to the database"
+        });
       });
-    });
+  }
 });
 
 server.delete("/api/users/:id", (req, res) => {
@@ -63,7 +70,15 @@ server.delete("/api/users/:id", (req, res) => {
   Db.remove(id)
     .then(deleted => {
       if (deleted === 1) {
-        res.status(204).end();
+        Db.find()
+          .then(users => {
+            res.status(200).json(users);
+          })
+          .catch(error => {
+            res
+              .status(500)
+              .json({ error: "The users information could not be retrieved." });
+          });
       } else {
         res
           .status(404)
@@ -78,27 +93,36 @@ server.delete("/api/users/:id", (req, res) => {
 server.put("/api/users/:id", (req, res) => {
   const id = req.params.id;
   const changes = req.body;
-
-  Db.update(id, changes)
-    .then(updated => {
-      if (updated) {
-        res.status(200).json(updated);
-      } else {
+  if (!changes.name || !changes.bio) {
+    res
+      .status(400)
+      .json({ errorMessage: "Please provide name and bio for the user." });
+  } else {
+    Db.update(id, changes)
+      .then(updated => {
+        if (updated) {
+          Db.findById(id)
+            .then(data => {
+              res.status(200).json(data);
+            })
+            .catch(error => {
+              res.status(500).json({
+                error:
+                  "There was an error while saving the user to the database"
+              });
+            });
+        } else {
+          res.status(404).json({
+            message: "The user with the specified ID does not exist."
+          });
+        }
+      })
+      .catch(error => {
         res
-          .status(404)
-          .json({ message: "The user with the specified ID does not exist." });
-      }
-      if (!changes) {
-        res
-          .status(400)
-          .json({ errorMessage: "Please provide name and bio for the user." });
-      }
-    })
-    .catch(error => {
-      res
-        .status(500)
-        .json({ error: "The user information could not be modified." });
-    });
+          .status(500)
+          .json({ error: "The user information could not be modified." });
+      });
+  }
 });
 
 const port = 8000;
